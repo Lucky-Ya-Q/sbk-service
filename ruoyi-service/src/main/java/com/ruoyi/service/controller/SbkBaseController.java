1cab6f1254cf2524f1a5d088408ed1ec53b4df5d
zkl <1981280278@qq.com>
2022-05-27 14:45:32 +0800

    物流信息查询

--- ruoyi-service/src/main/java/com/ruoyi/service/controller/SbkBaseController.java
+++ ruoyi-service/src/main/java/com/ruoyi/service/controller/SbkBaseController.java
@@ -2,6 +2,8 @@ package com.ruoyi.service.controller;
 
 import cn.hutool.core.codec.Base64;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.AES;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -10,7 +12,6 @@ import com.ruoyi.service.domain.SbkUser;
 import com.ruoyi.service.dto.FwmmxgParam;
 import com.ruoyi.service.dto.Result;
 import com.ruoyi.service.dto.RyjcxxbgParam;
-import com.ruoyi.service.service.CSBService;
 import com.ruoyi.service.service.SbkService;
 import com.ruoyi.service.service.WxArchivesService;
 import com.ruoyi.service.service.WxBukaInfoService;
@@ -143,9 +144,30 @@ public class SbkBaseController {
      * 制卡进度查询
      */
     @ApiOperation("制卡进度查询")
-    @PostMapping("/zkjdcx")
+    @GetMapping("/zkjdcx")
     public AjaxResult zkjdcx() {
         return AjaxResult.success();
+    }
+
+    /**
+     * 物流信息查询
+     */
+    @ApiOperation("物流信息查询")
+    @GetMapping("/wlxxcx")
+    public AjaxResult wlxxcx(String mailNum) {
+        AES aes = SecureUtil.aes("94DA411B9C39B410".getBytes());
+        String url = "http://ipps.hbwkd.cn/ipps/orderPay/api/EmsTrail/EmsTrailAction.do?actionType=getMailInfo";
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("AUTH_CODE", "94D5C8EAA808A9A5E050007F01005B3C");
+        hashMap.put("CUST_APPID", "wxde85bc4bf1f7629a");
+        hashMap.put("MAIL_NUM", mailNum);
+        String content = aes.encryptBase64(JSON.toJSONString(hashMap));
+        String result = restTemplate.postForObject(url, content, String.class);
+        if (result == null) {
+            return AjaxResult.error("没有查到物流信息");
+        }
+        result = aes.decryptStr(result.replaceAll("\\r\\n", ""));
+        return AjaxResult.success(JSON.parseObject(result));
     }
 
     /**
